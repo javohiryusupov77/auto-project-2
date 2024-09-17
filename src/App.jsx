925f6ca57af6f52a938f57b797309769a6472f4d
Yusupov Javohir <javohiryyusupov6@gmail.com>
2024-09-17 16:01:23 +0500

    commit last

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,83 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  useLocation,
+} from "react-router-dom";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import MainPage from "./pages/MainPage";
 import AboutCarsPage from "./pages/AboutCarsPage";
 import CarDetails from "./components/CarDetails";
 import { AutoCarsProvider } from "./components/AutoCarsContext";
+import PageTitle from "./components/PageTitle";
+import NotFoundPage from "./pages/NotFoundPage";
+
+function Layout({ children }) {
+  const location = useLocation();
+  const isNotFound = location.pathname === "/404" || location.pathname === "*";
+
+  return (
+    <div className="flex flex-col min-h-screen">
+      {!isNotFound && <Header />}
+      <div className="w-full max-w-[1440px] mx-auto flex-grow">
+        <div className="flex-grow flex justify-center">
+          <div className="w-full max-w-[1440px]">{children}</div>
+        </div>
+      </div>
+      {!isNotFound && <Footer />}
+    </div>
+  );
+}
 
 function App() {
   return (
     <AutoCarsProvider>
       <Router>
-        <div className="flex flex-col min-h-screen">
-          <div className="w-full max-w-[1440px] mx-auto flex-grow">
-            <Header />
-            <div className="flex-grow flex justify-center">
-              <div className="w-full max-w-[1440px]">
-                <Routes>
-                  <Route path="/" element={<MainPage />} />
-                  <Route path="/about-cars" element={<AboutCarsPage />} />
-                  <Route path="/about-cars/:id" element={<CarDetails />} />
-                </Routes>
-              </div>
-            </div>
-            <Footer />
-          </div>
-        </div>
+        <Layout>
+          <Routes>
+            <Route
+              path="/"
+              element={
+                <>
+                  <PageTitle title="Home" />
+                  <MainPage />
+                </>
+              }
+            />
+            <Route
+              path="/about-cars"
+              element={
+                <>
+                  <PageTitle title="About Cars" />
+                  <AboutCarsPage />
+                </>
+              }
+            />
+            <Route
+              path="/about-cars/:id"
+              element={
+                <>
+                  <PageTitle title="Car Details" />
+                  <CarDetails />
+                </>
+              }
+            />
+            <Route
+              path="*"
+              element={
+                <>
+                  <PageTitle title="404 | Page Not Found" />
+                  <NotFoundPage />
+                </>
+              }
+            />
+          </Routes>
+        </Layout>
       </Router>
     </AutoCarsProvider>
   );
 }
 
 export default App;
+
